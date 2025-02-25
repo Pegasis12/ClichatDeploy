b8d6dca6463f044076f2cf5fe84029e8871a4724
Pegasis12 <plsantana246@gmail.com>
2025-02-24 21:14:02 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 //menu header para ativar o after do a ao ser clicado
 const links = document.querySelectorAll(".header li a");
-function ativarLink(link) {
-  const url = location.href;
-  const href = link.href;
-  // metodo includes verifica se tem uma parte doq esta escrito no texto dentro de outro texto
-  if (url.includes(href)) {
-    link.classList.add("ativo");
-  }
-}
-links.forEach(ativarLink);
-
-//menu header para ativar o after do a ao ser clicado
 const links2 = document.querySelectorAll(".header-menu-second li a");
 function ativarLink(link) {
   const url = location.href;
@@ -20,6 +9,7 @@ function ativarLink(link) {
     link.classList.add("ativo");
   }
 }
+links.forEach(ativarLink);
 links2.forEach(ativarLink);
 
 //perguntas frequentes
@@ -40,3 +30,28 @@ function EventosPergunta(pergunta) {
 }
 
 perguntas.forEach(EventosPergunta);
+
+//vídeo loop
+document.addEventListener("DOMContentLoaded", function () {
+  const containers = document.querySelectorAll(".video-container");
+
+  containers.forEach(container => {
+    const video = container.querySelector("video");
+    const poster = container.querySelector(".poster");
+
+    container.addEventListener("mouseover", function () {
+      poster.style.opacity = 0; // Some com o poster suavemente
+      video.style.opacity = 1; // Aparece o vídeo
+      video.play();
+    });
+
+    container.addEventListener("mouseleave", function () {
+      video.pause();
+      video.currentTime = 0; // Reseta o vídeo
+      video.style.opacity = 0; // Some com o vídeo
+      poster.style.opacity = 1; // Traz o poster de volta
+    });
+  });
+});
+
+
